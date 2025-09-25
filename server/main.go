31d1ab10ea38b45ad3a9ad76236b9c5419c9246f
Sriram Venkatesh <vsriram353@gmail.com>
2025-09-25 19:48:41 +0530

    workout: add server wrapper using weather-cli/pkg/weather

---
+++ server/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+
+	"weather-cli/pkg/weather"
+)
+
+func weatherHandler(w http.ResponseWriter, r *http.Request) {
+	city := r.URL.Query().Get("city")
+	ctx, cancel := context.WithTimeout(r.Context(), 8*time.Second)
+	defer cancel()
+
+	resp, err := weather.GetWeather(ctx, city)
+	if err != nil {
+		http.Error(w, "failed to get weather: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(resp)
+}
+
+func main() {
+	http.HandleFunc("/weather", weatherHandler)
+	addr := ":8080"
+	log.Printf("server listening on %s", addr)
+	log.Fatal(http.ListenAndServe(addr, nil))
+}
